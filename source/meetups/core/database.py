5a0af98150848d50343b388f916cc5dc55235d42
Nicolas Serafini <nicolas.serafini@tunubi.com>
2020-07-16 02:08:39 -0300

    core database methods

---
+++ source/meetups/core/database.py
@@ -0,0 +1,22 @@
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+def get_engine(dsn: str):
+    return create_engine(dsn)
+
+def get_maker(dsn: str):
+    return sessionmaker(autocommit=False, autoflush=False, bind=get_engine(dsn))
+
+@contextmanager
+def get_session(dsn: str):
+    try:
+        maker = get_maker(dsn)
+        session = maker()
+        yield session
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
