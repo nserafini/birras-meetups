1e7061355fb277c9b1382d2bf24ac67136d0cc30
Nicolas Serafini <nicolas.serafini@tunubi.com>
2020-07-18 18:08:31 -0300

    role response model

--- source/meetups/models/role.py
+++ source/meetups/models/role.py
@@ -1,3 +1,6 @@
+from pydantic import BaseModel as BaseAPI
+from pydantic import constr
+
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
@@ -8,4 +11,7 @@ class Role(BaseModel):
     __tablename__ = "roles"
 
     name = Column(String(255), unique=True, nullable=False)
-    users = relationship("User", secondary="user_roles")
+
+class RoleOUT(BaseAPI):
+    id: constr(max_length=255)
+    name: constr(max_length=255)
