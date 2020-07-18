95b561493e75aa29dc84f80693e71bd344bb2595
Nicolas Serafini <nicolas.serafini@tunubi.com>
2020-07-18 04:13:59 -0300

    user service

---
+++ source/meetups/services/user.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Session
+from meetups.models.user import User
+
+class UserService:
+    @classmethod
+    def get_one(cls, db: Session, user_id: int):
+        user = db.query(User).get(user_id)
+        return user
