2989ef4419aef768fd7709ae91543f162b4f5aec
Nicolas Serafini <nicolas.serafini@tunubi.com>
2020-07-19 17:07:37 -0300

    app/ -> api/

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,6 @@
 set -e
 
 export APP_MODULE="meetups.app:app"
-export GUNICORN_CONF=/app/docker/app/gunicorn_conf.py
+export GUNICORN_CONF=/app/docker/api/gunicorn_conf.py
 
 exec gunicorn -k uvicorn.workers.UvicornWorker -c "$GUNICORN_CONF" "$APP_MODULE"
