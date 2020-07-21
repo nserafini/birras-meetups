e59565d5b574a454f77f8fe3684a4c8135dfcae9
Nicolas Serafini <nicolas.serafini@tunubi.com>
2020-07-21 03:05:13 -0300

    frot: app-module modules

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,21 +9,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { MeetupListComponent } from './components/meetup-list/meetup-list.component';
 import { CookieService } from 'ngx-cookie-service';
+import { ToobarComponent } from './components/toobar/toobar.component';
+import { MeetupCreateComponent } from './components/meetup-create/meetup-create.component';
+import { ToolbarAdminActionsComponent } from './components/toolbar-admin-actions/toolbar-admin-actions.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MeetupComponent,
-    MeetupListComponent
+    MeetupListComponent,
+    ToobarComponent,
+    MeetupCreateComponent,
+    ToolbarAdminActionsComponent
   ],
   imports: [
     BrowserModule,   
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [CookieService],
   bootstrap: [AppComponent]
