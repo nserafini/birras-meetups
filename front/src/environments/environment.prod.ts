2b8ee99ab269da668b1e6d91f21b2d7b34abfa88
Nicolas Serafini <nicolas.serafini@tunubi.com>
2020-07-22 02:20:13 -0300

    front: api url same hostname

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:5000',
+  API_URL: 'http://'+window.location.hostname+':5000',
   API_HEADERS: {
     headers: new HttpHeaders ({
       'X-API-Key': 'secret',
